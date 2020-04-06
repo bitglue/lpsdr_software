753f0216fe9a5985358d6877ad636817a200831a
Phil Frost <indigo@bitglue.com>
2020-04-05 17:22:56 -0700

    Set defaults for QRP Labs rig

--- src/rig/iq_audio_source.cc
+++ src/rig/iq_audio_source.cc
@@ -10,16 +10,21 @@ iq_audio_source::sptr iq_audio_source::make() {
 iq_audio_source_impl::iq_audio_source_impl()
     : gr::hier_block2("iq_audio_source", gr::io_signature::make(0, 0, 0),
                       gr::io_signature::make(1, 1, sizeof(gr_complex))) {
-  audio_source = gr::audio::source::make(sample_rate);
+  audio_source = gr::audio::source::make(sample_rate, "hw:CODEC", true);
   delay = gr::blocks::delay::make(sizeof(float), 0);
   float_to_complex = gr::blocks::float_to_complex::make();
   message_debug = gr::blocks::message_debug::make();
 
-  // 40m
-  iqbal_fix = gr::iqbalance::fix_cc::make(-0.0530295558, -0.300588399);
+  // Softrock 40m
+  // iqbal_fix = gr::iqbalance::fix_cc::make(-0.0530295558, -0.300588399);
 
-  // 20m
+  // Softrock 20m
   // iqbal_fix = gr::iqbalance::fix_cc::make(-0.111521058, -0.090425238);
+
+  // qrplabs 20m
+  iqbal_fix = gr::iqbalance::fix_cc::make(0.0322991163, -0.00278446102);
+
+  // iqbal_fix = gr::iqbalance::fix_cc::make(0.0, 0.0);
 
   iqbal_optimize = gr::iqbalance::optimize_c::make(0);
 
